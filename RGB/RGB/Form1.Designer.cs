8eb3a33e3266578f9362f960f50f3afd33e96dcd
Will Green <72352225+willgreen946@users.noreply.github.com>
2023-12-14 13:14:13 +0000

    Add files via upload

---
+++ RGB/RGB/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace RGB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RedSlider = new System.Windows.Forms.VScrollBar();
+            this.GreenSlider = new System.Windows.Forms.VScrollBar();
+            this.BlueSlider = new System.Windows.Forms.VScrollBar();
+            this.WinXPButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // RedSlider
+            // 
+            this.RedSlider.Location = new System.Drawing.Point(9, 87);
+            this.RedSlider.Maximum = 255;
+            this.RedSlider.Name = "RedSlider";
+            this.RedSlider.Size = new System.Drawing.Size(17, 354);
+            this.RedSlider.TabIndex = 1;
+            this.RedSlider.Scroll += new System.Windows.Forms.ScrollEventHandler(this.RedSlider_Scroll);
+            // 
+            // GreenSlider
+            // 
+            this.GreenSlider.Location = new System.Drawing.Point(59, 87);
+            this.GreenSlider.Maximum = 255;
+            this.GreenSlider.Name = "GreenSlider";
+            this.GreenSlider.Size = new System.Drawing.Size(17, 354);
+            this.GreenSlider.TabIndex = 2;
+            this.GreenSlider.Scroll += new System.Windows.Forms.ScrollEventHandler(this.GreenSlider_Scroll);
+            // 
+            // BlueSlider
+            // 
+            this.BlueSlider.Location = new System.Drawing.Point(111, 87);
+            this.BlueSlider.Maximum = 255;
+            this.BlueSlider.Name = "BlueSlider";
+            this.BlueSlider.Size = new System.Drawing.Size(17, 354);
+            this.BlueSlider.TabIndex = 3;
+            this.BlueSlider.Scroll += new System.Windows.Forms.ScrollEventHandler(this.BlueSlider_Scroll);
+            // 
+            // WinXPButton
+            // 
+            this.WinXPButton.Location = new System.Drawing.Point(29, 12);
+            this.WinXPButton.Name = "WinXPButton";
+            this.WinXPButton.Size = new System.Drawing.Size(75, 23);
+            this.WinXPButton.TabIndex = 4;
+            this.WinXPButton.Text = "Reset";
+            this.WinXPButton.UseVisualStyleBackColor = true;
+            this.WinXPButton.Click += new System.EventHandler(this.WinXPButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.WinXPButton);
+            this.Controls.Add(this.BlueSlider);
+            this.Controls.Add(this.GreenSlider);
+            this.Controls.Add(this.RedSlider);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.VScrollBar RedSlider;
+        private System.Windows.Forms.VScrollBar GreenSlider;
+        private System.Windows.Forms.VScrollBar BlueSlider;
+        private System.Windows.Forms.Button WinXPButton;
+    }
+}
+
--- RGB/RGB/Form1.cs
+++ RGB/RGB/Form1.cs
@@ -1,1 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
+namespace RGB
+{
+    public partial class Form1 : Form
+    {
+        int redValue = 0;
+        int blueValue = 0;
+        int greenValue = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        /*
+         * Changes the window color 
+         */
+        private void ChangeColor(int r, int g, int b)
+        {
+            Color myrgbColor = new Color();
+            myrgbColor = Color.FromArgb(r, g, b);
+            this.BackColor = myrgbColor;
+        }
+
+        private void BlueSlider_Scroll(object sender, ScrollEventArgs e)
+        {
+            blueValue = BlueSlider.Value;
+            ChangeColor(redValue, greenValue, blueValue);
+        }
+
+        private void GreenSlider_Scroll(object sender, ScrollEventArgs e)
+        {
+            greenValue = GreenSlider.Value;
+            ChangeColor(redValue, greenValue, blueValue);
+        }
+
+        private void RedSlider_Scroll(object sender, ScrollEventArgs e)
+        {
+            redValue = RedSlider.Value;
+            ChangeColor(redValue, greenValue, blueValue);
+        }
+
+        private void WinXPButton_Click(object sender, EventArgs e)
+        {
+            RedSlider.Value = 0;
+            GreenSlider.Value = 0;
+            BlueSlider.Value = 0;
+
+            ChangeColor(0, 0, 0);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ChangeColor(0, 0, 0);
+        }
+    }
+}
